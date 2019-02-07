22a4b27d6a52d2c0a51b4902ec3a60ca0c247186
Brewster Malevich <sbmalev@gmail.com>
2019-02-07 10:34:41 -0700

    Add logging and arg parsing to icesm download script

--- download_icesm.py
+++ download_icesm.py
@@ -1,14 +1,76 @@
 # Stupid wrapper for `globus-cli` to load cesm files from NCAR server.
+#
+# Can run from Bash with:
+#
+# python download_icesm.py \
+#     --casename b.e12.B1850C5.f19_g16.i21ka.03 \
+#     --downloadpath /xdisk/malevich/transferstuff
+#
+# See help with `python run_assimilation_report.py --help`.
+# Otherwise you're going to want to import and run through `main()`.
 
 import os
 import pathlib
 import subprocess
+import argparse
+import logging
 
 
-def transfer(from_dir, to_dir):
-    """Download all files in directory from_dir on NCAR Campaign to to_dir on HPC.
+log = logging.getLogger(__name__)
+
+
+def globus_find_endpoint(searchname, owner_id):
+    """Get globus endpoint ID based on name and owner_id.
+
+    Parameters
+    ----------
+    searchname : str
+        Endpoint name to search for.
+    owner_id : str
+        Endpoint owner name to filter results by.
+
+    Returns
+    -------
+    String endpoint ID or `None`.
+    """
+    log.debug('finding globus endpoint for {} ({})'.format(searchname, owner_id))
+    searchname = str(searchname)
+    owner_id = str(owner_id)
+    # In future might want to change `--format UNIX`, below, to something more
+    # machine-readable.
+    cmd_template = ["globus", "endpoint", "search", searchname,
+                    "--filter-owner-id", owner_id,
+                    "--jq", "DATA[0].id", "--format", "UNIX"]
+    response = subprocess.run(cmd_template, check=True, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+
+    endpoint_id = None
+    if response.returncode == 0 and response.returncode != b'None\n':
+        endpoint_id = response.stdout.rstrip().decode('utf-8')
+
+    log.debug('globus endpoint ID {}'.format(endpoint_id))
+    return endpoint_id
+
+
+def globus_transfer(from_endpoint, from_dir, to_endpoint, to_dir, task_label=None):
+    """Globus transfer from 'from' endpoint and dir to 'to endpoint and dir.
 
     Assumes you've alread run `globus login`.
+
+    Parameters
+    ----------
+    from_endpoint : str
+        Globus ID of endpoint containing data to transfer.
+    from_dir : str
+        Path to directory on 'from' endpoint containing all the data we will 
+        transfer.
+    to_endpoint : str
+        Globus ID of endpoint we want to transfer data to.
+    to_dir : str
+        Path to directory on 'to' endpoint we want to transfer data into.
+    task_label : str or None
+        Optional str label to give globus transfer task.
+
 
     Examples
     --------
@@ -17,84 +79,120 @@ def transfer(from_dir, to_dir):
     > to_d = '/xdisk/malevich/'
     > tranfer(from_d, to_d)
     """
+    from_endpoint = str(from_endpoint)
+    to_endpoint = str(to_endpoint)
+
     # In future might want to change `--format UNIX`, below, to something more
     # machine-readable.
+    cmd_template = ["globus", "transfer", "--recursive", "--sync-level", "mtime",
+                    "{}:{}".format(from_endpoint, from_dir),
+                    "{}:{}".format(to_endpoint, to_dir),
+                    "--jq", "task_id", "--format", "UNIX"]
 
-    # Endpoint we upload to
-    eplocal = subprocess.run(["globus", "endpoint", "search", "arizona#sdmz-dtn",
-                              "--filter-owner-id", "tmerritt@arizona.edu",
-                              "--jq", "DATA[0].id", "--format", "UNIX"],
-                             check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if eplocal.returncode == 0 and eplocal.returncode != b'None\n':
-        endpoint_local = eplocal.stdout.rstrip().decode('utf-8')
-    else:
-        raise KeyError('could not find local endpoint')
+    if task_label is not None:
+        cmd_template.append("--label")
+        cmd_template.append(str(task_label))
 
-    # Endpoint we download from
-    epremote = subprocess.run(["globus", "endpoint", "search", "NCAR Campaign Storage", "--filter-owner-id", "ncar@globusid.org", "--jq", "DATA[0].id", "--format", "UNIX", "--label", "download_icesm.py"],
-                              check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if epremote.returncode == 0 and epremote.returncode != b'None\n':
-        endpoint_remote = epremote.stdout.rstrip().decode('utf-8')
-    else:
-        raise KeyError('could not find remote endpoint')
-
-    # set REMOTE_DIR = ${ARCH_REM}/${casename}/${model}/proc/tseries/monthly/${var}
-    #  set LOCAL_DIR  = ${ARCH_LOC}/${casename}/${model}/proc/tseries/monthly/${var}
-
-    #  globus transfer --recursive --sync-level mtime                 \
-    #      ${EPSTORE}:${REMOTE_DIR} ${EPGLADE}:${LOCAL_DIR}           \
-    #      --jq task_id --format UNIX
-    subprocess.run(["globus", "transfer", "--recursive", "--sync-level", "mtime",
-                    "{}:{}".format(endpoint_remote, from_dir),
-                    "{}:{}".format(endpoint_local, to_dir),
-                    "--jq", "task_id", "--format", "UNIX"],
-                   check=True)
+    subprocess.run(cmd_template, check=True)
+    log.debug('created globus transfer task from {}:{} to {}:{}'.format(
+        from_endpoint, from_dir, to_endpoint, to_dir))
 
 
-# Use globus to load needed files from NCAR. Assumes you ran `globus login` from shell before:
-CASENAME = 'b.e12.B1850C5.f19_g16.i21ka.03'
-DOWNLOAD_PATH = os.getcwd()
-atm_variables = ['PRECC',
-                 'PRECL',
-                 'PRECSC',
-                 'PRECSL',
-                 'TS',
-                 'TREFHT',
-                 'PRECRC_H218Or',
-                 'PRECRL_H218OR',
-                 'PRECSC_H218Os',
-                 'PRECSL_H218OS',
-                 'PRECRC_H216Or',
-                 'PRECRL_H216OR',
-                 'PRECSC_H216Os',
-                 'PRECSL_H216OS',
-                 'PRECRC_H2Or',
-                 'PRECRL_H2OR',
-                 'PRECSC_H2Os',
-                 'PRECSL_H2OS',
-                 'PRECRC_HDOr',
-                 'PRECRL_HDOR',
-                 'PRECSC_HDOs',
-                 'PRECSL_HDOS',
-                 'OMEGA',
-                 'PS',
-                 ]
+def download_icesm(casename, download_path, atm_variables, ocn_variables):
+    """Start globus transfers of iCESM output from NCAR Campaign to UA HPC.
 
-ocn_variables = ['R18O',
-                 'TEMP',
-                 'SALT',
-                 ]
+    You should have run `globus login` from shell terminal before using this command.
 
+    Parameters
+    ----------
+    casename : str
+        iCESM experiment case name. Used to find the model run on NCAR.
+    download_path : str
+        UA HPC path we're transfering files to. Should be able to handle lots of data.
+        Path must already exist.
+    atm_variables : list of strs
 
-DOWNLOAD_PATH = pathlib.Path(DOWNLOAD_PATH)
-DOWNLOAD_PATH.mkdir(parents=True, exist_ok=True)
+    """
+    log.debug('begin creating globus transfers for {} to {}'.format(casename, download_path))
+    ncar_endpoint = globus_find_endpoint(
+        searchname='NCAR Campaign Storage', owner_id='ncar@globusid.org')
+    uahpc_endpoint = globus_find_endpoint(
+        searchname='arizona#sdmz-dtn', owner_id='tmerritt@arizona.edu')
 
-from_atm_template = '/gpfs/csfs1/univ/uazn0013/jiangzhu/archive/{}/atm/proc/tseries/monthly/{}/'
-for variable in atm_variables:
-    from_d = from_atm_template.format(CASENAME, variable)
-    transfer(from_dir=from_d, to_dir=DOWNLOAD_PATH)
+    assert ncar_endpoint is not None or uahpc_endpoint is not None, 'could not get globus endpoint IDs'
 
-from_ocn_template = '/gpfs/csfs1/univ/uazn0013/jiangzhu/archive/{}/ocn/proc/tseries/monthly/{}/'
-for variable in ocn_variables:
-    from_d = from_ocn_template.format(CASENAME, variable)
-    transfer(from_dir=from_d, to_dir=DOWNLOAD_PATH)
+    # Load files from atmosphere component
+    from_atm_template = '/gpfs/csfs1/univ/uazn0013/jiangzhu/archive/{}/atm/proc/tseries/monthly/{}/'
+    for variable in atm_variables:
+        from_d = from_atm_template.format(casename, variable)
+        globus_transfer(from_endpoint=ncar_endpoint, from_dir=from_d,
+                        to_endpoint=uahpc_endpoint, to_dir=download_path)
+
+    # Load files from ocean component
+    from_ocn_template = '/gpfs/csfs1/univ/uazn0013/jiangzhu/archive/{}/ocn/proc/tseries/monthly/{}/'
+    for variable in ocn_variables:
+        from_d = from_ocn_template.format(casename, variable)
+        globus_transfer(from_endpoint=ncar_endpoint, from_dir=from_d,
+                        to_endpoint=uahpc_endpoint, to_dir=download_path)
+    log.debug('globus transfers created')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Globus transfer iCESM files from NCAR to UA HPC')
+    parser.add_argument('--casename', metavar='CASENAME', nargs=1,
+                        default=['b.e12.B1850C5.f19_g16.i21ka.03'],
+                        help='iCESM experiment case name')
+    parser.add_argument('--downloadpath', metavar='DOWNLOADPATH', nargs=1,
+                        default=[None],
+                        help='directory files will be transferred to to. PWD is default. Will be created if it doesnt exist.')
+    parser.add_argument('--log', metavar='LOGPATH', nargs=1, default=[None],
+                        help='file path that log will be written to')
+    args = parser.parse_args()
+
+    if args.log[0] is not None:
+        logging.basicConfig(level=logging.DEBUG,
+                            filename=args.log[0],
+                            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    dl_path = None
+    if args.downloadpath[0] is None:
+        dl_path = os.getcwd()
+
+    casename = str(args.casename[0])
+    atm_variables = ['PRECC',
+                     'PRECL',
+                     'PRECSC',
+                     'PRECSL',
+                     'TS',
+                     'TREFHT',
+                     'PRECRC_H218Or',
+                     'PRECRL_H218OR',
+                     'PRECSC_H218Os',
+                     'PRECSL_H218OS',
+                     'PRECRC_H216Or',
+                     'PRECRL_H216OR',
+                     'PRECSC_H216Os',
+                     'PRECSL_H216OS',
+                     'PRECRC_H2Or',
+                     'PRECRL_H2OR',
+                     'PRECSC_H2Os',
+                     'PRECSL_H2OS',
+                     'PRECRC_HDOr',
+                     'PRECRL_HDOR',
+                     'PRECSC_HDOs',
+                     'PRECSL_HDOS',
+                     'OMEGA',
+                     'PS',
+                     ]
+
+    ocn_variables = ['R18O',
+                     'TEMP',
+                     'SALT',
+                     ]
+
+    # Create dl path if doesn't exist.
+    dl_path = pathlib.Path(dl_path)
+    dl_path.mkdir(parents=True, exist_ok=True)
+
+    download_icesm(casename=casename, download_path=dl_path,
+                   atm_variables=atm_variables, ocn_variables=ocn_variables)
