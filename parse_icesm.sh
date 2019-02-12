0877672a3b9d81eb7876c6834b127ab3f6d2a46e
Brewster Malevich <sbmalev@gmail.com>
2019-02-12 12:18:13 -0700

    Move main parse script to HPC fat nodes

--- parse_icesm.sh
+++ parse_icesm.sh
@@ -31,18 +31,18 @@
 ### Set the number of cores (cpus) and memory that will be used for this job
 ### When specifying memory request slightly less than 2GB memory per ncpus for standard node
 ### Some memory needs to be reserved for the Linux system processes
-#PBS -l select=1:ncpus=28:mem=168gb
+#PBS -l select=1:ncpus=6:mem=252gb:pcmem=42gb
 
 ### Important!!! Include this line for your 1p job.
 ### Without it, the entire node, containing 12 core, will be allocated
 #PBS -l place=pack:shared
 
 ### Specify "wallclock time" required for this job, hhh:mm:ss
-#PBS -l walltime=06:00:00
+#PBS -l walltime=12:00:00
 
 ### Specify total cpu time required for this job, hhh:mm:ss
 ### total cputime = walltime * ncpus
-#PBS -l cput=168:00:00
+#PBS -l cput=72:00:00
 
 ### Load required modules/libraries if needed (blas example)
 ### Use "module avail" command to list all available modules
